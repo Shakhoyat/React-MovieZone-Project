1d6cbe3a46dc6624e24127e88f79cda56967ff1d
Shakhoyat <shakoyatsujon@gmail.com>
2025-05-28 16:19:38 +0600

    added movie data for upcoming releases &popular films from 2024-2025

---
+++ MovieZone-vite-project/src/MovieXone/data.js
@@ -0,0 +1,152 @@
+export const movies = [
+  {
+    id: 693134,
+    title: "Dune: Part Two",
+    release_date: "2024-03-01",
+    backdrop_path:
+      "https://image.tmdb.org/t/p/original/15H1kMVIx82mJQRNRLJjVkkYMWR.jpg",
+    poster_path:
+      "https://image.tmdb.org/t/p/original/1pdfLvkbY9ohJlCjQH2CZjjYVvJ.jpg",
+    category: "Sci-Fi",
+  },
+  {
+    id: 402431,
+    title: "Wicked",
+    release_date: "2024-11-22",
+    backdrop_path:
+      "https://image.tmdb.org/t/p/original/uKb22E0nlzr914bA9KyA5CVCOlV.jpg",
+    poster_path:
+      "https://image.tmdb.org/t/p/original/c5Tqxeo1UpBvnAc3csUm7j3hlQl.jpg",
+    category: "Musical",
+  },
+  {
+    id: 762509,
+    title: "Mufasa: The Lion King",
+    release_date: "2024-12-20",
+    backdrop_path:
+      "https://image.tmdb.org/t/p/original/6rh8lWOGiMzNjLnxjt6z8V4yoTk.jpg",
+    poster_path:
+      "https://image.tmdb.org/t/p/original/lurEK87kukWNaHtu97jKpNMYhOc.jpg",
+    category: "Animation",
+  },
+  {
+    id: 748783,
+    title: "The Garfield Movie",
+    release_date: "2024-05-24",
+    backdrop_path:
+      "https://image.tmdb.org/t/p/original/2RVcJbWFmICRDsVxRI8F5xRmRsK.jpg",
+    poster_path:
+      "https://image.tmdb.org/t/p/original/p6AbOJvMQhBmffd0PIv0u8ghWeY.jpg",
+    category: "Animation",
+  },
+  {
+    id: 558449,
+    title: "Gladiator II",
+    release_date: "2024-11-15",
+    backdrop_path:
+      "https://image.tmdb.org/t/p/original/3Kzc6V4MWs3RXCmE4XFgrXBNcK7.jpg",
+    poster_path:
+      "https://image.tmdb.org/t/p/original/2cxhvwyEwRlysAmRH4iodkvo0z5.jpg",
+    category: "Action",
+  },
+  {
+    id: 1100782,
+    title: "Smile 2",
+    release_date: "2024-10-18",
+    backdrop_path:
+      "https://image.tmdb.org/t/p/original/tElnmtQ6yz1PjN1kePNl8yMSb59.jpg",
+    poster_path:
+      "https://image.tmdb.org/t/p/original/aE85MnPIsSoSs3978Noo16BRsKN.jpg",
+    category: "Horror",
+  },
+  {
+    id: 1034541,
+    title: "Terrifier 3",
+    release_date: "2024-10-11",
+    backdrop_path:
+      "https://image.tmdb.org/t/p/original/18TSJF1WLA4CkymvVUcKDBwUJ9F.jpg",
+    poster_path:
+      "https://image.tmdb.org/t/p/original/7NDHoebflLwL1CcgLJ9wZbbDrmV.jpg",
+    category: "Horror",
+  },
+  {
+    id: 912649,
+    title: "Venom: The Last Dance",
+    release_date: "2024-10-25",
+    backdrop_path:
+      "https://image.tmdb.org/t/p/original/3V4kLQg0kSqPLctI5ziYWabAZYF.jpg",
+    poster_path:
+      "https://image.tmdb.org/t/p/original/aosm8NMQ3UyoBVpSxyimorCQykC.jpg",
+    category: "Action",
+  },
+  {
+    id: 1184918,
+    title: "The Wild Robot",
+    release_date: "2024-09-27",
+    backdrop_path:
+      "https://image.tmdb.org/t/p/original/4zlOPT9CrtIX05bBIkYxNZsm5zN.jpg",
+    poster_path:
+      "https://image.tmdb.org/t/p/original/wTnV3PCVW5O92JMrFvvrRcV39RU.jpg",
+    category: "Animation",
+  },
+  {
+    id: 889737,
+    title: "Joker: Folie à Deux",
+    release_date: "2024-10-04",
+    backdrop_path:
+      "https://image.tmdb.org/t/p/original/uGmYqxh8flqkudioyFtD7IJSHxK.jpg",
+    poster_path:
+      "https://image.tmdb.org/t/p/original/if8QiqCI7WAGImKcJCfzp6VTyKA.jpg",
+    category: "Drama",
+  },
+  {
+    id: 1125311,
+    title: "Trap",
+    release_date: "2024-08-02",
+    backdrop_path:
+      "https://image.tmdb.org/t/p/original/ynu8bt9tWBy6AdlrDqqd0BHXYks.jpg",
+    poster_path:
+      "https://image.tmdb.org/t/p/original/jwoaKYVqPgYemFpaANL941EF94R.jpg",
+    category: "Thriller",
+  },
+  {
+    id: 748230,
+    title: "A Complete Unknown",
+    release_date: "2024-12-25",
+    backdrop_path:
+      "https://image.tmdb.org/t/p/original/96DeWCzn6k1XYF8gCFCWdhk5P7D.jpg",
+    poster_path:
+      "https://image.tmdb.org/t/p/original/4P5oOHKMtQy1MbA0ySUCkNaLnxo.jpg",
+    category: "Drama",
+  },
+  {
+    id: 1100099,
+    title: "Nosferatu",
+    release_date: "2024-12-25",
+    backdrop_path:
+      "https://image.tmdb.org/t/p/original/18k9U02V08lhf1YYhLFyOFpYlE.jpg",
+    poster_path:
+      "https://image.tmdb.org/t/p/original/qbJhKSdw59XTRjhRpB1hU6UqCYh.jpg",
+    category: "Horror",
+  },
+  {
+    id: 1226578,
+    title: "Longlegs",
+    release_date: "2024-07-12",
+    backdrop_path:
+      "https://image.tmdb.org/t/p/original/7aPrv2HFssWcOtpig5G3HEVk3uS.jpg",
+    poster_path:
+      "https://image.tmdb.org/t/p/original/5aj8vVGFwGVbQQs26ywhg4Zxk2L.jpg",
+    category: "Horror",
+  },
+  {
+    id: 1034062,
+    title: "Moana 2",
+    release_date: "2024-11-27",
+    backdrop_path:
+      "https://image.tmdb.org/t/p/original/tElnmtQ6yz1PjN1kePNl8yMSb59.jpg",
+    poster_path:
+      "https://image.tmdb.org/t/p/original/yh64qw9mgXBvlaWDi7Q9tpUBAvH.jpg",
+    category: "Animation",
+  },
+];
